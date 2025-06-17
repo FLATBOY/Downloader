205eaac1a21cae03e1dbeb2399e30003ae3f35cf
AKIRA <quanghy789@gmail.com>
2025-06-17 22:47:09 +0700

    Delete user_logs.sql

--- user_logs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_logs (
-  id SERIAL PRIMARY KEY,
-  ip VARCHAR(45),
-  format VARCHAR(10),
-  filename TEXT,
-  started_at TIMESTAMP,
-  finished_at TIMESTAMP,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
